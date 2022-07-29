71786990518920e6b00e1e6265c073d38fbce289
Harjasleen Malvai <hmalvai@fb.com>
2022-07-28 23:28:59 -0700

    Ran to a million

--- src/main.rs
+++ src/main.rs
@@ -22,10 +22,10 @@ type Blake3 = Blake3_256<BaseElement>;
 const LABEL_VALUE_SIZE_BYTES: usize = 32;
 
 /// Number of key entries in a large batch.
-const LARGE_BATCH_SIZE: u64 = 1000;
+const LARGE_BATCH_SIZE: u64 = 100000;
 
 /// Number of epochs equal to numebr of publish operations.
-const NUM_EPOCHS: u64 = 10;
+const NUM_EPOCHS: u64 = 101;
 
 /// csv file name to be prepended for the data
 const CSV_PREFIX: &str = "./output_csvs/ozks_experiment_";
@@ -49,7 +49,7 @@ pub async fn maybe_publish_multi_epoch(batch_size: u64, num_epoch: u64) {
         Option::from("root"),
         Option::from("example"),
         Option::from(8001),
-        Specific(Duration::new(120, 0)),
+        Specific(Duration::new(1200, 0)),
         200,
     )
     .await;
